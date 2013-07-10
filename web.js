1f625307037c027f7e2cfcadf1a41b17f0808aaa
Priya Rajkumar <priyami2010@gmail.com>
2013-07-10 02:44:26 +0000

    update

--- web.js
+++ web.js
@@ -3,10 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFile('index.html'),function(err,data){
-  if(err) throw err;
-  console.log(data);
-});
+    var content = fs.readFileSync('index.html');
+  response.send(content)
 });
 
 var port = process.env.PORT || 5000;
