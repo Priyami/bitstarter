f7bb0d3b9c6e4fcfba443b6015b97cc4a03e8b0a
Priya Rajkumar <priyami2010@gmail.com>
2013-07-10 11:06:58 +0000

    updated for error

--- web.js
+++ web.js
@@ -3,14 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    fs.readFile('/etc/passwd', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
-  response.send(data);
+  response.send('Hello');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
