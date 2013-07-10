d9d08ac6b2cfadac6421bb3f30df0e8ffebaf05a
Priya Rajkumar <priyami2010@gmail.com>
2013-07-10 13:31:36 +0000

    update

--- web.js
+++ web.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-
+var content;
 app.get('/', function(request, response) {
     fs.readFile('index.html', function (err, data) {
 if (err)
 throw err;
 if (data)
  console.log(data.toString('utf8'));
-	//response.send(data.toString('utf8'));
+ content = data.toString('utf8');
+	response.send(content);
 	});
-    response.send(data.toString('utf8'));
+    response.send(content);
     response.send('hello');
 });
 
