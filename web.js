4430368ed0c624ffbe1bfd49ee33e7ba22dba6af
Priya Rajkumar <priyami2010@gmail.com>
2013-07-10 12:31:48 +0000

    At first hello world

--- web.js
+++ web.js
@@ -1,29 +1,10 @@
 var express = require('express');
-var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    readIndexHtmlfile('index.html', function(err,indexContents){
-
-	console.log(indexContents);
-    }
-
-  response.send('Hello');
+  response.send('Hello World!');
 });
-
-function readIndexHtmlfile(pah,callback){
-    try{
-	var filename = require.resolve(path)
-	fs.readfile(filename,'utf8',callback);
-
-    }catch(e){
-	callback(e);
-    }
-
-}
-
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
